923b15c3719af490d12988d13b14039cb7ff2320
walterokpala <uchewalter8@gmail.com>
2023-02-02 12:40:30 -0500

    added handle bar (templating engine )

--- express-project/controller/message.controller.js
+++ express-project/controller/message.controller.js
@@ -1,7 +1,11 @@
+const path = require("path");
+
 function getAMessage(req, res) {
-  res.send(
-    "<html><body><ul><li> Hello Albert!!!!!!!!!!!!!!!!!!!</li></ul></body></html>"
-  );
+  const pathToFile = path.join(__dirname, "..", "public", "networkChuck.png");
+  res.sendFile(pathToFile);
+  // res.send(
+  //   "<html><body><ul><li> Hello Albert!!!!!!!!!!!!!!!!!!!</li></ul></body></html>"
+  // );
 }
 
 module.exports = {
--- express-project/server.js
+++ express-project/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 
 const messageRouter = require("./routes/message.route");
 const friendsRouter = require("./routes/friends.route");
@@ -13,7 +14,16 @@ app.use((req, res, next) => {
   console.log(`${req.method} ${req.baseUrl}${req.url} ${delta} ms`);
 });
 app.use(express.json());
+app.use("/site", express.static(path.join(__dirname, "public")));
 app.use("/friends", friendsRouter);
 app.use("/messages", messageRouter);
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
+app.get("/", (req, res) => {
+  res.render("index", {
+    title: "My Friends are clever",
+    caption: "Linux Guru Channel",
+  });
+});
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
